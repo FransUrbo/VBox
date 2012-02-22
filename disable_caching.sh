50348990eaa7210b36ef075eb412cc6058a51f39
Turbo Fredriksson <turbo@bayour.com>
2012-02-22 10:31:22 +0100

    Add IDE support to disable.

--- disable_caching.sh
+++ disable_caching.sh
@@ -8,11 +8,13 @@ VBoxManage -nologo list --long vms | \
     while read vm; do
 	VBoxManage -nologo showvminfo "$vm" | grep iqn | \
 	    while read ctrl; do
+		dev=`echo "$ctrl" | sed 's@.* (\([0-9]\),.*@\1@'`
+		lun=`echo "$ctrl" | sed 's@.*, \([0-9]\)).*@\1@'`
+		echo "$vm: $dev/$lun"
 		if echo "$ctrl" | grep -q ^SATA; then
-		    dev=`echo "$ctrl" | sed 's@.* (\([0-9]\),.*@\1@'`
-		    lun=`echo "$ctrl" | sed 's@.*, \([0-9]\)).*@\1@'`
-		    echo "$vm: $dev/$lun"
 		    VBoxManage setextradata "$vm" "VBoxInternal/Devices/ahci/$dev/LUN#$lun/Config/IgnoreFlush" 0
+		elif echo "$ctrl" | grep -q ^IDE; then
+		    VBoxManage setextradata "$vm" "VBoxInternal/Devices/piix3ide/$dev/LUN#$lun/Config/IgnoreFlush" 0
 		fi
 	    done
     done
